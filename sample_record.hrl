22acc0ac4d4862392f06838183468caee98f14c7
Robert Schonberger <rschonberger@gmail.com>
2009-03-29 08:51:34 +1100

    Add a random seed to the hrl.
    Use a random seed in state for random numbers rather than using the
    process dictionary random.

--- sample_record.hrl
+++ sample_record.hrl
@@ -2,4 +2,4 @@
 %% Author: Robert Schonberger. 
 %% Copyright 2009 Robert Schonberger
 %% See the MIT-LICENSE file for licensing information.
--record(sample, {table, seen, total}). 
+-record(sample, {table, seen, total, seed}). 
